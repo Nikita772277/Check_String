abf597a8a56864667318ece5675286ebbe00db69
Студент 3 < 3@DESKTOP-PS97MJ6>
2022-12-20 11:28:44 +0300

    Добавьте файлы проекта.

---
+++ CheckString/Checks.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckString
+{
+    internal class Checks
+    {
+        public bool Alphanumeric(string word)
+        {
+            List<int> ints = new List<int>();
+            if(word==""|| string.IsNullOrEmpty(word))
+            {
+                return false;
+            }
+            char[] chars = word.ToCharArray();
+            for(int i = 0; i < chars.Length; i++)
+            {
+                if (char.IsSeparator(chars[i])||char.IsSymbol(chars[i])|| char.IsPunctuation(chars[i]))
+                {
+                    return false;
+                }                
+                if (char.IsNumber(chars[i]))
+                {                    
+                    int temp= Convert.ToInt32(chars[i].ToString());                    
+                    ints.Add(temp);
+                    string combinedString = string.Join("", ints);
+                    bool fka = int.TryParse(combinedString, out var fkajsf);
+                    if (fkajsf < 0 || fkajsf > 9)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ CheckString/Program.cs
@@ -0,0 +1,5 @@
+﻿using CheckString;
+using System;
+Checks checks = new Checks();
+var result=checks.Alphanumeric("10");
+Console.WriteLine(result);
